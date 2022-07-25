24c96a2c42efcbf0fd48282969c05516313ef8b5
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-25 13:45:29 +0200

    Created a basic react native app

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+      <Text>Another text</Text>
+      </View>
+      <Text>Heyyyyy</Text> 
+      <Button title='Tap me' />
     </View>
   );
 }
